d6e7cd1a482a0fa01c5ba5275615cd8a3e24dc43
Rémi <remi.olier9@gmail.com>
2020-05-27 11:57:02 +0200

    Simple registration process

--- models/PostManager.php
+++ models/PostManager.php
@@ -4,10 +4,14 @@ include_once "PDO.php";
 function GetOnePostFromId($id)
 {
   global $PDO;
-  $response = $PDO->query("SELECT * FROM post WHERE id = $id");
+  $response = $PDO->prepare("SELECT * FROM comment WHERE id = :id ");
+  $response->execute(
+    array(
+      "id" => $id
+    )
+  );
   return $response->fetch();
 }
-
 function GetAllPosts()
 {
   global $PDO;
@@ -21,11 +25,17 @@ function GetAllPosts()
 function SearchInPosts($search)
 {
   global $PDO;
-  $response = $PDO->query(
+  $response = $PDO->prepare(
     "SELECT post.*, user.nickname "
       . "FROM post LEFT JOIN user on (post.user_id = user.id) "
-      . "WHERE post.content like '%$search%'"
+      . "WHERE content like :search "
       . "ORDER BY post.created_at DESC"
+  );
+  $searchWithPercent = "%$search%";
+  $response->execute(
+    array(
+      "search" => $searchWithPercent
+    )
   );
   return $response->fetchAll();
 }
@@ -39,5 +49,11 @@ function GetAllPostsFromUserId($userId)
 function CreateNewPost($userId, $msg)
 {
   global $PDO;
-  $response = $PDO->query("INSERT INTO post(user_id, content) values ($userId, '$msg')");
+  $response = $PDO->prepare("INSERT INTO post(user_id, content) values (:userId, :msg)");
+  $response->execute(
+    array(
+      "userId" => $userId,
+      "msg" => $msg
+    )
+  );
 }
--- models/UserManager.php
+++ models/UserManager.php
@@ -32,3 +32,28 @@ function GetUserIdFromUserAndPassword($username, $password)
     return -1;
   }
 }
+
+function IsNicknameFree($nickname)
+{
+  global $PDO;
+  $response = $PDO->prepare("SELECT * FROM user WHERE nickname = :nickname ");
+  $response->execute(
+    array(
+      "nickname" => $nickname
+    )
+  );
+  return $response->rowCount() == 0;
+}
+
+function CreateNewUser($nickname, $password)
+{
+  global $PDO;
+  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+  $response->execute(
+    array(
+      "nickname" => $nickname,
+      "password" => $password
+    )
+  );
+  return $PDO->lastInsertId();
+}
